de8f492ae6d70b587193881f7121d25a250374eb
HellaxA <abereznikov64@gmail.com>
2026-08-02 16:13:53 +0100

    added unit tests for split function

--- src/helpers/helpers.cpp
+++ src/helpers/helpers.cpp
@@ -1,21 +1,32 @@
 #include <vector>
+#include <iostream>
 #include <string>
 
 std::vector<std::string> split(const std::string& str, const std::string& delimeter) 
 {
     std::vector<std::string> result;
+    if (str.empty() || delimeter.empty()) {
+        return result;
+    }
 
     size_t start = 0;
     size_t end = str.find(delimeter);
+    size_t del_length = delimeter.length();
 
     while (end != std::string::npos) 
     {
         std::string element = str.substr(start, end - start);
+
         result.push_back(element);
-        start = end + delimeter.length();
-        end = str.find(delimeter);
+
+        end += del_length;
+        start = end;
+        end = str.find(delimeter, end);
     }
 
-    result.push_back(str.substr(start));
+    if (start < str.length()) {
+        result.push_back(str.substr(start));
+    }
+
     return result;
 }
--- tests/helpers/test_helpers.cpp
+++ tests/helpers/test_helpers.cpp
@@ -1,7 +1,33 @@
 #include "../../src/helpers/helpers.hpp"
 #include <gtest/gtest.h>
 
-TEST(SanityCheck, BasicAssertion) {
-    EXPECT_EQ(1+1, 2);
+TEST(split, simple_space_string) {
+    std::vector<std::string> actual = split("foo bar gaz", " ");
+    std::vector<std::string> expected {"foo", "bar", "gaz"};
+    EXPECT_EQ(expected, actual);
 }
 
+TEST(split, crlf_delimeter_string) {
+    std::vector<std::string> actual = split(" foo\r\nbar\r\ngaz\r\n", "\r\n");
+    std::vector<std::string> expected {" foo", "bar", "gaz"};
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(split, array_bulk_string) {
+    std::vector<std::string> actual = split("*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n", "\r\n");
+    std::vector<std::string> expected {"*2", "$4", "ECHO", "$3", "hey"};
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(split, empty_string) {
+    std::vector<std::string> actual = split("", "\r\n");
+    std::vector<std::string> expected {};
+    EXPECT_EQ(expected, actual);
+}
+
+// might fix in the future
+TEST(split, empty_delimeter) {
+    std::vector<std::string> actual = split("foo bar", "");
+    std::vector<std::string> expected {};
+    EXPECT_EQ(expected, actual);
+}
